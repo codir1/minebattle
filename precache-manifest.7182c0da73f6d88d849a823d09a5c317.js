0c4576dd06df3ff0639b5421fdc419931429e367
codir1 <74709012+codir1@users.noreply.github.com>
2020-11-29 15:01:16 +0300

    Add files via upload

---
+++ precache-manifest.7182c0da73f6d88d849a823d09a5c317.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a24dace768af817e4a74b3977833ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9526733d28f814afab85",
+    "url": "/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "eb77854616c8f56d40da",
+    "url": "/static/css/main.4af94eda.chunk.css"
+  },
+  {
+    "revision": "9526733d28f814afab85",
+    "url": "/static/js/2.f5890b1f.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/static/js/2.f5890b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb77854616c8f56d40da",
+    "url": "/static/js/main.133e429b.chunk.js"
+  },
+  {
+    "revision": "bc2a1405ed9ab3c4d883",
+    "url": "/static/js/runtime-main.ba958da7.js"
+  },
+  {
+    "revision": "65c72b15d6bf4d9259b8d767773c894f",
+    "url": "/static/media/double.65c72b15.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d02205633dae9f409245d47ff909583.js"
+  "/precache-manifest.7182c0da73f6d88d849a823d09a5c317.js"
 );
 
 self.addEventListener('message', (event) => {
